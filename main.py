a2f1a2d21431c2b03d78706472149766172eb9d7
oyakovenko <alex231@ukr.net>
2023-10-07 20:32:34 +0300

    add db, fill table

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QComboBox, \
-    QMainWindow, QTableWidget
+    QMainWindow, QTableWidget, QTableWidgetItem
 import sys
+import sqlite3
 
 
 class MainWindow(QMainWindow):
@@ -21,10 +22,19 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
 
+    def load_data(self):
+        connect = sqlite3.connect("database.db")
+        result = connect.execute("select * from students")
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+        connect.close()
 
-    # def load_data(self):
 
 
 
@@ -32,4 +42,7 @@ class MainWindow(QMainWindow):
 app = QApplication(sys.argv)
 age_calculator = MainWindow()
 age_calculator.show()
+age_calculator.load_data()
 sys.exit(app.exec())
+
+
